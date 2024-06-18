a9ad1ff2d778345bc3518ca48ecd00d4da1d69b7
Jack <jacksonfilho3@gmail.com>
2024-06-18 17:41:25 -0300

    Refactoring tests

--- src/routes/reactionRoutes.js
+++ src/routes/reactionRoutes.js
@@ -6,7 +6,7 @@ function createReactionRoutes(reactionController) {
     const router = express.Router();
     router.get('/', reactionController.getReactions.bind(reactionController));
     router.post('/', validateSchema(createReactionsSchema), reactionController.createReaction.bind(reactionController));
-    router.get('/', validateSchema(authorizationSchema), reactionController.getReactionById.bind(reactionController));
+    router.get('/:id', validateSchema(authorizationSchema), reactionController.getReactionById.bind(reactionController));
     router.put('/:id', validateSchema(updateReactionsSchema), reactionController.updateReaction.bind(reactionController));
     router.delete('/:id', validateSchema(getByIdSchema), reactionController.deleteReaction.bind(reactionController));
     return router
